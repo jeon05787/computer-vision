a63731eea0693e318daf043df817d50632b37fed
sOngwOn <127367796+jeon05787@users.noreply.github.com>
2026-03-05 19:13:39 +0900

    Update 01_gray.py

--- E01_OpenCV/01_gray.py
+++ E01_OpenCV/01_gray.py
@@ -2,20 +2,26 @@ import cv2 as cv
 import numpy as np
 import sys
 
+# 이미지 파일 읽기
 img = cv.imread('C:/Users/COM/Desktop/computer_vision/image/soccer.jpg')
 
+#이미지가 정상적으로 로드되지 않았으면 종료
 if img is None:
     sys.exit('파일을 찾을 수 없습니다.')
 
+#BGR 컬러 이미지를 그레이스케일로 변환
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-
+#np.hstack()로 원본과 gray(1채널)를 붙이려면 채널 수가 같아야 함
 # gray → BGR로 변환 (채널 맞추기)
 gray_bgr = cv.cvtColor(gray, cv.COLOR_GRAY2BGR)
 
-# 가로 연결
+# 가로 연결 (원본 왼쪽, 그레이스케일 오른쪽)
 result = np.hstack((img, gray_bgr))
 
+#결과 창에 출력
 cv.imshow('Original vs Gray', result)
 
+#키 입력을 기다렸다가 아무키나 누르면 종료
 cv.waitKey(0)
+#열린 모든 OpenCV창 닫기
 cv.destroyAllWindows()
